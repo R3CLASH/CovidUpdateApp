868a1dbd228303f81a917497b66e1635d0429797
Thavisha <thavisha@surge.global>
2023-01-19 18:47:48 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Axios from "axios";
+import React, { useState } from "react";
+import Header from "./Components/Header";
+import LocalData from "./Components/LocalData";
+import GlobalData from "./Components/GlobalData";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // Api Documentation Link: www.hpb.health.gov.lk/en/api-documentation
+  https: return (
+    <Router>
+      <div className="App min-h-screen">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<GlobalData />} />
+          <Route exact path="/local" element={<LocalData />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <div className="px-5 py-10">
@@ -9,6 +9,20 @@ function Header() {
       <h3 className="text-left text-2xl pt-3  text-gray-700 lg:text-center">
         It's your responsibility to Stay Updated!
       </h3>
+      <div className="flex justify-center pb-2 pt-10">
+        <div className="flex justify-center w-auto px-2 py-2 bg-gray-200 gap-2 rounded-xl">
+          <NavLink to="/">
+            <button className="bg-white w-40 px-5 py-2 rounded-xl text-center font-medium text-lg">
+              Global
+            </button>
+          </NavLink>
+          <NavLink to="/local">
+            <button className="bg-white w-40 px-5 py-2 rounded-xl text-center font-medium text-lg">
+              Local
+            </button>
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
